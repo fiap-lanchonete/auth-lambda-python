f86f164de01dd5c2ec58fcf4ae116bb129513608
gfrancodev <68759427+gfrancodev@users.noreply.github.com>
2024-01-29 01:51:12 -0300

    fix: auth credentials

--- code/packages/main.py
+++ code/packages/main.py
@@ -89,4 +89,4 @@ def lambda_handler(event, context):
 if __name__ == "__main__":
     with app.app_context():
         db.create_all()
-        app.run(debug=True, host='0.0.0.0', port=80)
+    app.run(debug=True, host='0.0.0.0', port=80)
